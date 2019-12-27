357e9e2b85eef0a860b58133e96bd0941e468d3e
Alexey Tolpinskiy <tvl1607@mail.ru>
2019-12-27 14:02:43 +0300

    Исправил ошибку с дробными числами

--- HW-1.4.py
+++ HW-1.4.py
@@ -4,18 +4,18 @@
 # Далее запросите численность сотрудников фирмы и определите прибыль фирмы в расчете на одного сотрудника.
 
 
-proceed = int(input('Введите выручку'))
-costs = int(input('Введите издержки: '))
+proceed = float(input('Введите выручку: '))
+costs = float(input('Введите издержки: '))
 profit = proceed - costs
 
 if profit <= costs:
     print("Ваши издержки слишком высоки")
 elif profit > costs:
     print("Ваша выручка выше ваших издержки")
-    prifitab = profit // costs
-    employ = int(input('Введите колличество сотрудников в компании: '))
-    profit_emp = profit // employ
-    print("Прибыль на одного работника составляет", profit_emp)
+    prifitab = profit / costs
+    employ = float(input('Введите колличество сотрудников в компании: '))
+    profit_emp = (profit) / (employ)
+    print(f"Прибыль на одного работника составляет - {profit_emp:.3f}")
 
 
 
