3fc9b58d4659ebb9eac668f02b56364641fa452f
Alexey Tolpinskiy <tvl1607@mail.ru>
2019-12-28 18:18:32 +0300

    Доделал 2.2

--- HW_2.2.py
+++ HW_2.2.py
@@ -2,17 +2,17 @@
 # Значениями обмениваются элементы с индексами 0 и 1, 2 и 3 и т.д.
 # При нечетном количестве элементов последний сохранить на своем месте.
 # Для заполнения списка элементов необходимо использовать функцию input().
-my_list = list(input("Введите что-нибудь"))
+my_list = list(input("Введите что-нибудь: "))
 
 if len(my_list) % 2 == 0:
     i = 0
     while i < len(my_list):
         t = my_list[i]
-        my_list[i] = my_list[i+1]
-        my_list[i+1] = t
+        my_list[i] = my_list[i + 1]
+        my_list[i + 1] = t
         i += 2
     print(my_list)
-elif len(my_list) % 2 != 0:
+else:
     i = 0
     while i < len(my_list) - 1:
         t = my_list[i]
@@ -20,7 +20,3 @@ elif len(my_list) % 2 != 0:
         my_list[i + 1] = t
         i += 2
     print(my_list)
-
-
-
-
