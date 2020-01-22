5487cbac1ebf8c5eab60fbdaf75092f4ea482b49
Alexey Tolpinskiy <tvl1607@mail.ru>
2020-01-22 13:39:15 +0300

    доделал

--- HW_4.7.py
+++ HW_4.7.py
@@ -3,3 +3,19 @@
 # Функция отвечает за получение факториала числа, а в цикле необходимо выводить только первые 15 чисел.
 #Подсказка: факториал числа n — произведение чисел от 1 до n. Например, факториал четырёх 4! = 1 * 2 * 3 * 4 = 24.
 
+from itertools import count
+from math import factorial
+
+def fibo_gen():
+    for el in count(1):
+        yield factorial(el)
+
+
+generator = fibo_gen()
+x = 0
+for i in generator:
+    if x == 15:
+        break
+    else:
+        x += 1
+        print(f"Факториал{x} = {i}")
